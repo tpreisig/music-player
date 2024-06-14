c2fca304cf758688456edcaf874767b52a2fb190
tpreisig <90914512+tpreisig@users.noreply.github.com>
2024-06-14 16:08:52 +0200

    Song list for the music player

---
+++ script.js
@@ -0,0 +1,20 @@
+const songList = [
+    {
+        name: "Merstonbianchi",
+        artist: "Media Stuff Productions",
+        src: "./assets/sound1.mp3",
+        cover: "./assets/cover1.jpeg"
+    },
+    {
+        name: "Muhcoweri",
+        artist: "Muh Line Productions",
+        src: "assets/sound2.mp3",
+        cover: "assets/cover2.jpeg"
+    },
+    {
+        name: "Tricaramelli",
+        artist: "Truffle Productions",
+        src: "assets/sound3.mp3",
+        cover: "assets/cover3.jpeg"
+    }
+];
